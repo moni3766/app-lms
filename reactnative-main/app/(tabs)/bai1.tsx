49cae9c86e17e7567a308ad1386f25615402112b
moni3766 <roaphio@gmail.com>
2026-08-28 23:06:05 +0700

    Cap nhat code bai 1

---
+++ reactnative-main/app/(tabs)/bai1.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Bai1() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
